5b455dff63f1f01d6c5ba17e5f707009dc72db96
waterlulu12 <aatishshrestha123@gmail.com>
2023-02-04 14:08:40 +0545

    csv data included

---
+++ menu.js
@@ -0,0 +1,27 @@
+// const { app, Menu } = require("electron");
+
+// const isMac = process.platform === "darwin";
+// const template = [
+//   ...(isMac) ?
+// ]
+// const mainProcess = require("./main");
+
+// const template = [
+//   {
+//     label: "Edit",
+//     submenu: [
+//       {
+//         label: "Copy",
+//         accelerator: "CommandOrControl+C",
+//         role: "copy",
+//       },
+//       {
+//         label: "Paste",
+//         accelerator: "CommandOrControl+V",
+//         role: "paste",
+//       },
+//     ],
+//   },
+// ];
+
+// module.exports = Menu.buildFromTemplate(template);
